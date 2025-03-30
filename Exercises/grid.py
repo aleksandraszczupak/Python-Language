6449fb410dd0658a7cf83882ba5f3177ebc44d1f
Aleksandra Szczupak <aleszczupak@gmail.com>
2025-03-30 15:56:23 +0200

    Create grid.py

---
+++ Exercises/grid.py
@@ -0,0 +1,31 @@
+'''Write a program that draws a rectangle made of small squares. Build
+a full string and then print it out.'''
+
+rows = int(input('Number of rows:\n'))
+cols = int(input('Number of cols:\n'))
+
+# solution 1
+aline = '+'
+bline = '|'
+grid = ''
+for i in range(cols):
+    aline += '---+'
+    bline += '   |'
+for i in range(rows):
+    grid += aline + '\n' + bline + '\n'
+grid += aline
+print(grid)
+
+# solution 2
+print()
+aline = cols * '+---' + '+\n'
+bline = cols * '|   ' + '|\n'
+grid = rows * (aline + bline) + aline
+print(grid)
+
+# solution 3
+print()
+row1 = '---'.join('+' * (cols+1)) + '\n'
+row2 = '   '.join('|' * (cols+1)) + '\n'
+grid = row2.join([row1] * (rows+1))
+print(grid)
